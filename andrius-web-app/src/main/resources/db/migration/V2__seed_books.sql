9a8e52c44e6221aa0e6802d337639a373a677fea
Andrius Stulgys <72042720+Andrew1415@users.noreply.github.com>
2026-03-08 20:27:41 +0200

    added

---
+++ andrius-web-app/src/main/resources/db/migration/V2__seed_books.sql
@@ -0,0 +1,5 @@
+INSERT INTO books (title, author, publish_year) VALUES
+('Clean Code', 'Robert C. Martin', 2008),
+('Refactoring', 'Martin Fowler', 2018),
+('Domain-Driven Design', 'Eric Evans', 2003),
+('Designing Data-Intensive Applications', 'Martin Kleppmann', 2017);
